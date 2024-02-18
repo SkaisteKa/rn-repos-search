13aed3e6d01738f6efbcafdc6a38d2df2d2d52aa
Skaiste Kankalyte <skaiste.ka@gmail.com>
2024-02-18 22:09:40 +0200

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        printWidth: 80,
+        singleQuote: true,
+        quoteProps: 'consistent',
+        jsxSingleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
 };
