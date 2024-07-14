9ab6db6bf85ad550320bf5c82ece98f74061c5d2
Skaiste Kankalyte <skaiste.ka@gmail.com>
2024-07-14 14:28:52 +0300

    status bar style

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import IssueSearchScreen from './src/screens';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -8,7 +8,8 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <SafeAreaView>
+      <SafeAreaView style={styles.safeAreaStyles}>
+        <StatusBar barStyle='light-content' />
         <IssueSearchScreen />
       </SafeAreaView>
     </QueryClientProvider>
@@ -16,3 +17,7 @@ const App = () => {
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  safeAreaStyles: { backgroundColor: '#040C28' },
+});
